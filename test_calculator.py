c612bc9c42f5da454f721dc7d952b551a1cc5af4
Rauulito <7rnavarro7@gmail.com>
2022-09-29 14:20:34 +0200

    clase test calculator metida

--- test_calculator.py
+++ test_calculator.py
@@ -6,16 +6,10 @@ from calculator import Calculator
 # Creamos una clase heredando de TestCase
 class TestMyCalculator(unittest.TestCase):
 # Creamos una prueba para probar un valor inicial
+    def setUp(self):
+        self.calc = Calculator()
     def test_initial_value(self):
-        def setUp(self):
-            self.calc = Calculator()
-        def test_initial_value(self):
-            self.assertEqual(0, self.calc.value)
-# Creamos un nuevo test para comprobar una suma
-        def test_add_method(self):
-# Ejecutamos el método
-            self.calc.add(1, 3)
-# Comprobamos si el valor es el que esperamos
-            self.assertEqual(4, self.calc.value)
+        self.assertEqual(0, self.calc.value)
+
 
 
